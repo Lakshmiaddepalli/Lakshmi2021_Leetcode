b8cc15bcda8c56cb16cdff984dfcf14038c8940e
Lakshmi Addepalli <lakshi.ana@gmail.com>
2020-12-06 19:25:17 -0500

    working commit - Created using LeetHub

--- find-all-the-lonely-nodes/find-all-the-lonely-nodes.java
+++ find-all-the-lonely-nodes/find-all-the-lonely-nodes.java
@@ -14,31 +14,44 @@
  * }
  */
 class Solution {
+    /*
+            1
+         /     \
+        2       3
+          \    /  
+           4   5
+           
+          if both child are null -> leaf node
+          if one chile returns a value and other child does not -> then that parent has a lonely child.
+           
+          May be:
+            1. write a
+           
+    */
+    List<Integer> result = new ArrayList<>();
     
-    List<Integer> ls;
+    public void checkChild(TreeNode root){
+        if(root.left == null && root.right == null){
+            return;
+        }else if(root.left != null && root.right != null){
+            checkChild(root.left);// 2
+            checkChild(root.right);// 3
+            
+        }else if(root.left != null || root.right != null){
+            if(root.left != null){
+                result.add(root.left.val);
+                checkChild(root.left);
+            }else{
+                result.add(root.right.val);
+                checkChild(root.right);
+            }
+        }
+        return;
+        
+    }
     
     public List<Integer> getLonelyNodes(TreeNode root) {
-        ls = new ArrayList<Integer>();
-        dfs(root);
-        return ls;
+        checkChild(root);// 1
+        return result;
     }
-    
-    public void dfs(TreeNode root){
-        
-        if(root == null){
-            return;
-        }
-        
-        if(root.right == null && root.left != null){
-            ls.add(root.left.val);
-        }
-        
-        if(root.left == null && root.right != null){
-            ls.add(root.right.val);
-        }
-        
-        dfs(root.left);
-        dfs(root.right);
-    }
-    
 }
