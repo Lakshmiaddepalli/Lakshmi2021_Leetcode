92158a76a518acf30a2263c4cf03d01079335e81
Lakshmi Addepalli <lakshi.ana@gmail.com>
2021-01-07 01:58:09 -0500

    Time: 0 ms (100.00%), Space: 39.4 MB (43.26%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.java
+++ merge-two-binary-trees/merge-two-binary-trees.java
@@ -22,10 +22,8 @@ class Solution {
             return t1;
         }
         t1.val += t2.val;
-        
         t1.left = mergeTrees(t1.left, t2.left);
         t1.right = mergeTrees(t1.right, t2.right);
-        
         return t1;
     }
 }
