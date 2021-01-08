ec8ee5745660932d1ce5aae6a48857bd407d5857
Lakshmi Addepalli <sla410@nyu.edu>
2021-01-07 23:45:21 -0500

    Update search-in-a-binary-search-tree.java

--- search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -30,3 +30,16 @@ class Solution {
         
     }
 }
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root!=null && root.val!=val){
+            if (root.val > val){
+                root = root.left;
+            }else{
+                root = root.right;
+            }
+        }
+        return root;
+    }
+}
