66a22c6285e38abba7000f1cc9c9d8d906ae112b
Lakshmi Addepalli <lakshi.ana@gmail.com>
2021-01-29 08:53:12 -0500

    Time: 3 ms (60.72%), Space: 38.6 MB (38.27%) - LeetHub

---
+++ rotting-oranges/rotting-oranges.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair<Integer, Integer>> queue = new ArrayDeque();
+
+        // Step 1). build the initial set of rotten oranges
+        int freshOranges = 0;
+        int ROWS = grid.length, COLS = grid[0].length;
+
+        for (int r = 0; r < ROWS; ++r)
+            for (int c = 0; c < COLS; ++c)
+                if (grid[r][c] == 2)
+                    queue.offer(new Pair(r, c));
+                else if (grid[r][c] == 1)
+                    freshOranges++;
+
+        // Mark the round / level, _i.e_ the ticker of timestamp
+        queue.offer(new Pair(-1, -1));
+
+        // Step 2). start the rotting process via BFS
+        int minutesElapsed = -1;
+        int[][] directions = { {-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> p = queue.poll();
+            int row = p.getKey();
+            int col = p.getValue();
+            if (row == -1) {
+                // We finish one round of processing
+                minutesElapsed++;
+                // to avoid the endless loop
+                if (!queue.isEmpty())
+                    queue.offer(new Pair(-1, -1));
+            } else {
+                // this is a rotten orange
+                // then it would contaminate its neighbors
+                for (int[] d : directions) {
+                    int neighborRow = row + d[0];
+                    int neighborCol = col + d[1];
+                    if (neighborRow >= 0 && neighborRow < ROWS && 
+                        neighborCol >= 0 && neighborCol < COLS) {
+                        if (grid[neighborRow][neighborCol] == 1) {
+                            // this orange would be contaminated
+                            grid[neighborRow][neighborCol] = 2;
+                            freshOranges--;
+                            // this orange would then contaminate other oranges
+                            queue.offer(new Pair(neighborRow, neighborCol));
+                        }
+                    }
+                }
+            }
+        }
+
+        // return elapsed minutes if no fresh orange left
+        return freshOranges == 0 ? minutesElapsed : -1;
+    }
+}
